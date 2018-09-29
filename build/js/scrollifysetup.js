25858219e8ca9579e19e24c8e89baeb186d1fc09
CalebBertrand <caleb.a.bertrand@gmail.com>
2018-09-29 10:23:52 -0500

    Harder, Better, Faster, Stronger

--- build/js/scrollifysetup.js
+++ build/js/scrollifysetup.js
@@ -4,10 +4,17 @@ $(function() {
 			scrollSpeed: 1400,
 			easing: "easeOutExpo",
 			before:function(i) {
-
+				if (i >= 4) {
+					$(".background-image").attr("src", "imgs/buildingplans.png");
+				}
+				console.log(i);
 			},
 			after:function(i) {
-
+				if (i <= 3) {
+					$(".background-image").attr("src", "../imgs/AFTObackground1.jpg");
+				}
+				console.log(i);
+				
 			},
 			setHeights: true
 		});
